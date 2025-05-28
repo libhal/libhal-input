4a56dd459195b84834e3dfcf56a8370c152f7383
Khalil Estell <kammce0@gmail.com>
2025-05-27 17:29:01 -0700

    :recycle: libhal-lpc40 -> libhal-arm-mcu/lpc40

--- demos/platforms/stm32f103c8.cpp
+++ demos/platforms/stm32f103c8.cpp
@@ -14,11 +14,11 @@
 
 #include <libhal-arm-mcu/dwt_counter.hpp>
 #include <libhal-arm-mcu/startup.hpp>
+#include <libhal-arm-mcu/stm32f1/clock.hpp>
+#include <libhal-arm-mcu/stm32f1/constants.hpp>
+#include <libhal-arm-mcu/stm32f1/output_pin.hpp>
+#include <libhal-arm-mcu/stm32f1/uart.hpp>
 #include <libhal-arm-mcu/system_control.hpp>
-#include <libhal-stm32f1/clock.hpp>
-#include <libhal-stm32f1/constants.hpp>
-#include <libhal-stm32f1/output_pin.hpp>
-#include <libhal-stm32f1/uart.hpp>
 #include <libhal-util/bit_bang_i2c.hpp>
 #include <libhal/units.hpp>
 
